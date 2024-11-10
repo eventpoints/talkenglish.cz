f035ef78c6ef1c669e465e89e12a3ba63b3950a3
kez <kerrialbeckettnewham@gmail.com>
2024-11-10 14:34:25 +0100

    fix rector break

--- rector.php
+++ rector.php
@@ -13,5 +13,4 @@ return RectorConfig::configure()
         __DIR__ . '/tests',
     ])
     // uncomment to reach your current PHP version
-     ->withPhpSets(php83: true)
     ->withTypeCoverageLevel(900);
