57a91a206851233af9042145c049dd9643c4bd19
kez <kerrialbeckettnewham@gmail.com>
2024-11-14 18:23:05 +0100

    add description to admin Quiz

--- src/Controller/Admin/QuizCrudController.php
+++ src/Controller/Admin/QuizCrudController.php
@@ -8,6 +8,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class QuizCrudController extends AbstractCrudController
@@ -22,6 +23,7 @@ class QuizCrudController extends AbstractCrudController
         return [
             IdField::new('id')->onlyOnIndex(),
             TextField::new('title'),
+            TextareaField::new('description'),
             ChoiceField::new('categoryEnum'),
             ChoiceField::new('levelEnum'),
             NumberField::new('timeLimitInMinutes'),
