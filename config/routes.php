49a65bf5c6a0ef4def772984553a316ac4e10d39
kez <kerrialbeckettnewham@gmail.com>
2024-11-18 17:41:25 +0100

    add root dir redirect to quizzes

--- config/routes.php
+++ config/routes.php
@@ -2,9 +2,19 @@
 
 declare(strict_types=1);
 
+use Symfony\Bundle\FrameworkBundle\Controller\RedirectController;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return static function (RoutingConfigurator $routingConfigurator): void {
+
+    $routingConfigurator->add('index_redirect', '/')
+        ->controller(RedirectController::class)
+        ->defaults([
+            'route' => 'quizzes',
+            'permanent' => true,
+            'keepQueryParams' => true,
+            'keepRequestMethod' => true,
+        ]);
 
     $routingConfigurator->import([
         'path' => '../src/Controller/Controller',
