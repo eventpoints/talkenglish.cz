cb2d42491718f0d042eb5f838d6e3ca018a2dd90
kez <kerrialbeckettnewham@gmail.com>
2025-01-13 10:31:41 +0100

    fix cs

--- src/Controller/Controller/QuizController.php
+++ src/Controller/Controller/QuizController.php
@@ -28,7 +28,6 @@ use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 
 #[Route(path: '/quizzes')]
